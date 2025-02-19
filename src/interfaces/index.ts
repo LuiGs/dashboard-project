bb3bc9a3dbc2bada36fa12d39b76dfda2d3abbbd
LuiGs <luiwildean@gmail.com>
2025-02-18 23:14:47 -0300

    Exportacion de Category Interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,7 +2,6 @@
 
 
 export * from './address.interface';
-export * from './category.interface';
 export * from './country.interface';
 export * from './paypal.interface';
 export * from './product.interface';
